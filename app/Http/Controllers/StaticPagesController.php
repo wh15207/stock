8b1562979e19b9ae3c24e2f6744ecf357df3e91f
Wu Hao <wh15207@163.com>
2019-07-23 10:52:47 +0800

    基础页面

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -9,14 +9,14 @@ class StaticPagesController extends Controller
 {
     //
     public function home(){
-        return "主页";
+        return view('static_pages/home');
     }
 
     public function help(){
-        return "帮助页";
+        return view('static_pages/help');
     }
 
     public function about(){
-        return "关于我们";
+        return view('static_pages/about');
     }
 }
