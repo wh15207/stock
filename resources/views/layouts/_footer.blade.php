5a3efc98814d6ef0d494a836cf1f96376f92225c
Wu Hao <wh15207@163.com>
2019-07-23 14:24:07 +0800

    局部视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <img src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png" class="brand-icon" alt="">
+    <a href="https://learnku.com/laravel/courses" target="_blank">
+        刻意练习，每日精进
+    </a>
+    <div class="float-right">
+        <a href="/about">关于</a>
+    </div>
+</footer>
