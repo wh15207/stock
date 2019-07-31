8d2cbf442614926f070b51b349378a1eb134bea2
WuHao <wh15207@163.com>
2019-07-31 09:58:17 +0800

    用户显示页面

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -8,6 +9,11 @@ use App\Http\Controllers\Controller;
 class UsersController extends Controller
 {
     public function create(){
-        return view('user.create');
+        return view('users.create');
     }
+    public function show(User $user){
+        return view('users.show',compact('user'));
+
+    }
+
 }
