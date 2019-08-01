c74ce6650b10031869cfcc56c0a2de7ebd19afb5
WuHao <wh15207@163.com>
2019-08-01 12:17:37 +0800

    更改用户资料

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <title>@yield('title','stock App')- Laravel 新手入门教程</title>
-    <link rel="stylesheet" href="../css/app.css">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
     @include("layouts._header")
@@ -14,6 +14,6 @@
             @include("layouts._footer")
         </div>
     </div>
-    <script src="../js/app.js"></script>
+    <script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
